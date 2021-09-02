9a03fd22ce8cede4266bf791b5ea2339bfe5f965
Rodrigo Bossini <31605875+professorbossini@users.noreply.github.com>
2021-09-02 01:02:41 +0000

    Implementamos a função que aumenta o vetor e a exibição.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 typedef struct {
   int * elementos;
   int qtde;
@@ -8,10 +8,6 @@ typedef struct {
 } lista;
 
 
-void inserir (lista * l, int e){
- l->elementos[l->qtde] = e;
- l->qtde++;
-}
 
 int esta_cheia (lista * l){
   return l->qtde >= l->cap;
@@ -27,8 +23,47 @@ lista * obter_lista (){
   return l;
 }
 
+void aumentar (lista * l){
+  int * aux = malloc(l->cap * 2 * sizeof(int));
+  for (int i = 0; i < l->qtde; i++){
+    aux[i] = l->elementos[i];
+  }
+  l->cap *= 2;
+  free(l->elementos);
+  l->elementos = aux;
+}
+
+void inserir (lista * l, int e){
+  if (esta_cheia (l))
+    aumentar(l);
+  l->elementos[l->qtde] = e;
+  l->qtde++;
+}
+
+void exibir (lista * l){
+  printf ("**********************\n");
+  printf ("Qtde: %d\n", l->qtde);
+  printf ("Cap: %d\n", l->cap);
+  for (int i = 0; i < l->qtde; i++){
+    printf ("%d ", l->elementos[i]);
+  }
+  printf ("\n");
+}
+
+
 
 int main(void) {
-  printf("Hello World\n");
+
+  //data fixa: 23/02/2001
+  //unidade de medida: dia
+
+  //2
+  srand(time(NULL));
+  lista * uma_lista = obter_lista();
+  for (int i = 0; i < 100; i++){
+    int dado = rand() % 20 + 1;
+    inserir (uma_lista, dado);
+    exibir(uma_lista);
+  }
   return 0;
 }
